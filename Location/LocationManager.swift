1edb3da4316fc0d28a0b4feac0e6c0c6a9607111
Roderic Linguri <linguri@digices.com>
2017-03-31 07:31:19 -0600

    Create LocationManager.swift

---
+++ Location/LocationManager.swift
@@ -0,0 +1,77 @@
+//
+// LocationManager.swift
+// DVM-Boilerplate
+//
+//  Created by Digices LLC on 3/30/17.
+//  Copyright © 2017 Digices LLC. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+protocol LocationManagerDelegate {
+  func didUpdateLocation()
+  func didChangeAuthorizationStatus()
+  func didSaveLocationData()
+}
+
+class LocationMangager : NSObject, CLLocationManagerDelegate {
+  
+  // MARK: - Properties
+  
+  var delegate : LocationManagerDelegate?
+  
+  let data : LocationData
+  
+  // MARK: - NSObject Methods
+  
+  override init() {
+    self.data = AppManager.shared.data.location
+  }
+
+  // MARK: - CLLocationManagerDelegate Methods
+
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+    // extract coordinates
+    let coords = locations[0].coordinate
+    self.data.latitude = coords.latitude
+    self.data.longitude = coords.longitude
+
+    // notify delegate if we have one
+    if let d = self.delegate {
+      d.didUpdateLocation()
+    }
+
+  }
+
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+    // how do you want to handle this?
+    if CLLocationManager.authorizationStatus() != .authorizedAlways {
+      manager.requestAlwaysAuthorization()
+    }
+
+    // notify delegate if we have one
+    if let d = self.delegate {
+      d.didChangeAuthorizationStatus()
+    }
+
+  }
+
+  // MARK: - Manager Methods
+
+  func save() {
+
+    // ask the AppManager to save all its data
+    let am = AppManager.shared
+    am.save()
+    
+    // notify delegate if we have one
+    if let d = self.delegate {
+      d.didSaveLocationData()
+    }
+
+  }
+
+}
